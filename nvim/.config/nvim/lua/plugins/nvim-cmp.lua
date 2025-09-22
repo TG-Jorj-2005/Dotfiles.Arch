6388b44367b0b31d3c5888aac4611b90e6e6b8ec
TG-Jorj-2005 <tanase.george.1905@gmail.com>
2025-09-22 16:50:09 +0300

    Mon Sep 22 04:50:09 PM EEST 2025

--- nvim/.config/nvim/lua/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -1,15 +1,19 @@
 return {
-  "hrsh7th/nvim-cmp",
-  opts = function(_, opts)
-    local cmp = require("cmp")
-    opts.mapping = vim.tbl_extend("force", opts.mapping, {
-      ["<Tab>"] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          cmp.confirm({ select = true }) -- confirmă sugestia
-        else
-          fallback()
-        end
-      end, { "i", "s" }),
-    })
-  end,
+  {
+    "hrsh7th/nvim-cmp",
+    enabled = true, -- forțăm activarea
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      opts.mapping = vim.tbl_extend("force", opts.mapping or {}, {
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.confirm({ select = true }) -- confirmă sugestia cu Tab
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+      })
+      return opts
+    end,
+  },
 }
